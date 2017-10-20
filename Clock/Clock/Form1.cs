ae63518aaa072dc41b74b76f49424052446b7a98
lindaliu <liuting_lliu@hotmail.com>
2017-10-20 22:04:48 +0800

    add clock

---
+++ Clock/Clock/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timers = System.Timers;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timers.Timer(500);
+            timer.Elapsed += timer_Elapsed;
+        }
+
+        void timer_Elapsed(object sender, Timers.ElapsedEventArgs e)
+        {
+            this.BeginInvoke(new Action(() => { this.label1.Text = DateTime.Now.ToString("HH:mm:ss"); }));
+        }
+        Timers.Timer timer = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+    }
+}
